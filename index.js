92ad2764bcb3322915dae43ca02a94ee9b68ad1b
mojustmo-glitch <maureen.ngugi@student.moringaschool.com>
2023-03-30 23:39:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  
+function scuberGreetingForFeet(a){
+  if (a <= 400){
+    return "This one is on me!"
+  }
+  else if (a >= 2000 && a <= 2499){
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(c){
+  let city;
+  c === "NYC" ? (city = "Ok, sounds good.") : (city = "No go.")
+  return city
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(t){
+  let resp;
+  switch (t) {
+    case 'generous':
+      resp = 'Thank you so much.';
+        break;
+    case 'not as generous':
+      resp = 'Thank you.';
+        break;
+    default:
+      resp = "Bye.";
+        break;
 }
+  return resp;
+}
+
+
+
+
+
+
+
+
